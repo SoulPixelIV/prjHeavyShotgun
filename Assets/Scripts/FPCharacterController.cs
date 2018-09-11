9bae001f8bad5f9dd6ed8cae8586aea34d1898d0
SoulPixelIV <william.dj@outlook.de>
2018-09-11 18:01:32 +0200

    Added Unity Project

---
+++ Assets/Scripts/FPCharacterController.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+
+public class FPCharacterController : MonoBehaviour
+{
+    //Crosshair
+    Rect crosshairRect;
+    public Texture crosshairTexture;
+    public float crosshairPercentage = 0.05f;
+
+    [Header("Movement")]
+    public float speedCam;
+    public float jumpSpeed;
+    public float gravity;
+    public float tiltRange;
+    public int jumpNum;
+    public float dashLength;
+
+    float speedMov;
+    float rotV = 0;
+    bool duckLock = true;
+    float verVelocity;
+    int jumpCount;
+    float dashTime;
+
+    [Header("Audio")]
+    public AudioSource audio1;
+    public AudioSource audio2;
+    private float stepSpeed;
+
+    CharacterController cc;
+
+    void Start()
+    {
+        Cursor.visible = false;
+
+        //Character Controller
+        cc = GetComponent<CharacterController>();
+
+        //Reset
+        jumpCount = jumpNum;
+        dashTime = dashLength;
+
+        //Crosshair
+        float crosshairSize = Screen.width * crosshairPercentage / 100;
+        crosshairRect = new Rect(Screen.width / 2 - crosshairSize / 2,
+                                 Screen.height / 2 - crosshairSize / 2,
+                                 crosshairSize, crosshairSize);
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //Mouse Movement
+        float rotH = Input.GetAxis("Mouse X") * speedCam;
+        rotV -= Input.GetAxis("Mouse Y") * speedCam;
+        rotV = Mathf.Clamp(rotV, -tiltRange, tiltRange);
+        transform.Rotate(0, rotH, 0);
+        if (Camera.main != null)
+        {
+            UnityEngine.Camera.main.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
+        }
+
+        //Movement
+        float forwardSpeed = Input.GetAxis("Vertical") * speedMov;
+        float sideSpeed = Input.GetAxis("Horizontal") * speedMov;
+
+        //Sprint
+        if (Input.GetKey(KeyCode.LeftShift) && duckLock == true)
+        {
+            speedMov = 10.7f;
+            stepSpeed = 2.5f;
+        }
+        else
+        {
+            speedMov = 7.5f;
+            stepSpeed = 1.6f;
+        }
+
+        //Duck
+        if (Input.GetKeyDown(KeyCode.LeftControl) && cc.isGrounded)
+        {
+            duckLock = false;
+            Vector3 duck = new Vector3(0, -1.5f, 0);
+            UnityEngine.Camera.main.transform.position = Vector3.Lerp(UnityEngine.Camera.main.transform.position, UnityEngine.Camera.main.transform.position + duck, 25f * Time.deltaTime);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl) && duckLock == false)
+        {
+            duckLock = true;
+            Vector3 duck = new Vector3(0, 1.5f, 0);
+            UnityEngine.Camera.main.transform.position = Vector3.Lerp(UnityEngine.Camera.main.transform.position, UnityEngine.Camera.main.transform.position + duck, 25f * Time.deltaTime);
+        }
+
+        //Gravity
+        verVelocity -= gravity * Time.deltaTime;
+        Vector3 speed = new Vector3(sideSpeed, verVelocity, forwardSpeed);
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && jumpNum != 0)
+        {
+            verVelocity = jumpSpeed;
+            jumpNum -= 1;
+        }
+        speed = transform.rotation * speed;
+        cc.Move(speed * Time.deltaTime);
+        if (cc.isGrounded)
+        {
+            jumpNum = jumpCount;
+        }
+
+        //Wallrunning
+        /*
+        if (cc.collisionFlags == CollisionFlags.Sides && forwardSpeed != 0 && sideSpeed != 0)
+        {
+            //Camera.main.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, 20, 0), Time.deltaTime);
+            verVelocity = -0.3f;
+            jumpNum = jumpCount + 1;
+        }
+        */
+
+        //Dash
+        if (Input.GetKey(KeyCode.F) && dashLength > 0)
+        {
+            dashLength -= Time.deltaTime;
+            speedMov = 50f;
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            dashLength = dashTime;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(crosshairRect, crosshairTexture);
+    }
+}
---
+++ Assets/Scripts/ShootingBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehaviour : MonoBehaviour {
+
+    public Rigidbody bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animator anim = GetComponent<Animator>();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("shoot"))
+            {
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot ()
+    {
+        Animator anim = GetComponent<Animator>();
+        
+        Rigidbody spawnedBullet;
+        anim.Play("shoot");
+        if (!gameObject.GetComponent<AudioSource>().isPlaying)
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        spawnedBullet = Instantiate(bullet, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z), Quaternion.identity);
+        spawnedBullet.velocity = transform.TransformDirection(new Vector3(-1, 0, 0) * 60);
+    }
+}
