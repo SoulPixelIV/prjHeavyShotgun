d3fef23a3cf4ebf3a6b5b15c6365b1e7ee245b8b
SoulPixelIV <william.dj@outlook.de>
2018-09-12 17:06:00 +0200

    Woodboxes and enemies now react to shots

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+
+    public int health;
+    public bool dead;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Health: " + health);
+	}
+
+    public bool HealthLoss (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            dead = true;
+        }
+        return dead;
+    }
+}
--- Assets/Scripts/ShootingBehaviour.cs
+++ Assets/Scripts/ShootingBehaviour.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class ShootingBehaviour : MonoBehaviour {
 
     public Rigidbody bullet;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int damage;
+    public int power;
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,14 +24,51 @@ public class ShootingBehaviour : MonoBehaviour {
     void Shoot ()
     {
         Animator anim = GetComponent<Animator>();
+        RaycastHit hit;
+
+        var direction = Camera.main.transform.TransformDirection(new Vector3(0, 0, 1));
         
-        Rigidbody spawnedBullet;
+        if (Physics.Raycast (Camera.main.transform.position, direction, out hit, 300))
+        {
+            GameObject objHit = hit.collider.gameObject;
+            Debug.Log("Hit object:" + objHit);
+
+            //Environment Hit
+            if (objHit.tag == "Environment")
+            {
+                //Direction between player and object
+                Vector3 dir = (transform.position - objHit.transform.position).normalized;
+
+                if (objHit.GetComponent<Destructible>() != null)
+                {
+                    objHit.GetComponent<Destructible>().Destroy();
+                }
+
+                objHit.GetComponent<Rigidbody>().AddForce(-dir * power, ForceMode.Impulse);
+            }
+            //Enemy Hit
+            if (objHit.tag == "Enemy")
+            {
+                //Direction between player and enemy
+                Vector3 dir = (transform.position - objHit.transform.position).normalized;
+
+                if (objHit.GetComponent<HealthSystem>().HealthLoss(damage) == true)
+                {
+                    objHit.GetComponent<Rigidbody>().AddForce(-dir * power, ForceMode.Impulse);
+                }
+                else
+                {
+                    objHit.GetComponent<Rigidbody>().AddForce(-dir * power / 8, ForceMode.Impulse);
+                    objHit.GetComponent<HealthSystem>().HealthLoss(damage);
+                }
+            }
+        }
+        
+        //Play animation and sound
         anim.Play("shoot");
         if (!gameObject.GetComponent<AudioSource>().isPlaying)
         {
             gameObject.GetComponent<AudioSource>().Play();
         }
-        spawnedBullet = Instantiate(bullet, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z), Quaternion.identity);
-        spawnedBullet.velocity = transform.TransformDirection(new Vector3(-1, 0, 0) * 60);
     }
 }
