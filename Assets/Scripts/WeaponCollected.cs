852d086210469d56e35f6e66d5b6a280abc9dc1c
SoulPixelIV <william.dj@outlook.de>
2018-09-19 19:02:31 +0200

    Added weapon pick up

---
+++ Assets/Scripts/WeaponCollected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollected : MonoBehaviour {
+
+    public int weapon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (weapon == 1)
+            {
+                GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<WeaponSwitching>().WeaponSwitch(1);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour {
+
+    public GameObject hands;
+    public GameObject shotgun;
+
+	// Use this for initialization
+	void Start () {
+        hands = GameObject.FindGameObjectWithTag("Hands").gameObject;
+        shotgun = GameObject.FindGameObjectWithTag("Shotgun").gameObject;
+
+        WeaponSwitch(0);
+    }
+	
+	// Update is called once per frame
+	public void WeaponSwitch (int weapon) {
+        if (weapon == 0)
+        {
+            hands.SetActive(true);
+            shotgun.SetActive(false);
+        }
+        if (weapon == 1)
+        {
+            hands.SetActive(false);
+            shotgun.SetActive(true);
+        }
+    }
+}
