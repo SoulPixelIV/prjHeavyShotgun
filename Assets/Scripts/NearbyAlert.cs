b16b40d3f33ff0756263ab45115f951d9acee3be
SoulPixelIV <william.dj@outlook.de>
2018-09-26 16:53:40 +0200

    Added nearbyAlert hitbox to all zombies

---
+++ Assets/Scripts/NearbyAlert.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearbyAlert : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (other.GetComponentInChildren<SightChecking>() != null)
+            {
+                if (other.GetComponentInChildren<SightChecking>().aggro)
+                {
+                    GetComponentInParent<SightChecking>().aggro = true;
+                }
+            }
+        }
+    }
+}
