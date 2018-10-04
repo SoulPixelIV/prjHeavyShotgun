2bd08e71ebfda16ee4dfbae4729d928c81e73eee
SoulPixelIV <william.dj@outlook.de>
2018-10-04 23:26:06 +0200

    Added ammo woodbox

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class Destructible : MonoBehaviour {
 
     public GameObject destroyedVersion;
-	
-	public void Destroy () {
+    public bool marked;
+    public Material standardMat;
+
+    private void Start()
+    {
+        if (marked)
+        {
+            GetComponent<Renderer>().material = standardMat;
+        }
+    }
+
+    public void Destroy () {
         Instantiate(destroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);
 	}
