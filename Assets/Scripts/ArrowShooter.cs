e7d52cf88a82c94aa94d8c0ccb32e7b60b4bfe9e
SoulPixelIV <william.dj@outlook.de>
2018-10-06 15:03:30 +0200

    Added arrow shooter

---
+++ Assets/Scripts/ArrowShooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooter : MonoBehaviour {
+
+    public bool activated;
+    public float shootTimer;
+    public GameObject arrow;
+    public GameObject spawnpoint;
+    public Vector3 direction;
+    public Quaternion rotation;
+
+    float shootTimerSave;
+
+	// Use this for initialization
+	void Start () {
+        shootTimerSave = shootTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated)
+        {
+            shootTimer -= 1 * Time.deltaTime;
+        }
+        if (shootTimer <= 0)
+        {
+            Shoot();
+            shootTimer = shootTimerSave;
+        }
+	}
+
+    void Shoot()
+    {
+        arrow = Instantiate(arrow, spawnpoint.transform.position, Quaternion.identity);
+        arrow.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
+        arrow.transform.rotation = rotation;
+    }
+}
