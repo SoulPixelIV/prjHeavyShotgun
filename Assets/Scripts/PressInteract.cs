3da2946078a55ef684147c1672d94316a9e1de62
SoulPixelIV <william.dj@outlook.de>
2019-01-06 19:13:45 +0100

    Added lever + continued work on level

---
+++ Assets/Scripts/PressInteract.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressInteract : MonoBehaviour {
+
+    public GameObject interactTxt;
+    public string openAnimation;
+    public bool access;
+    public bool showTxt = true;
+	
+	// Update is called once per frame
+	void Update () {
+		if (access)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Animator>().Play("" + openAnimation);
+                showTxt = false;
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (showTxt)
+            {
+                interactTxt.SetActive(true);
+            }
+            access = true;         
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            interactTxt.SetActive(false);
+            access = false;
+        }
+    }
+}
