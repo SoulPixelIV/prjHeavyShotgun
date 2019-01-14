6bcf5593dac3022cb3f28ef86515dd10af8a55a4
SoulPixelIV <william.dj@outlook.de>
2019-01-14 23:31:24 +0100

    Added skadi attack script

---
+++ Assets/Scripts/SkadiBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkadiBehaviour : MonoBehaviour {
+
+    public float attackCooldown;
+    public float hitboxCooldown;
+
+    float attackCooldownSave;
+    float hitboxCooldownSave;
+    bool punched;
+
+	// Use this for initialization
+	void Start () {
+        attackCooldownSave = attackCooldown;
+        hitboxCooldownSave = hitboxCooldown;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (attackCooldown > 0)
+        {
+            attackCooldown -= 1 * Time.deltaTime;
+        }
+
+        if (punched)
+        {
+            hitboxCooldown -= 1 * Time.deltaTime;
+        }
+        if (hitboxCooldown <= 0)
+        {
+            //transform.Find("HandsAttackHitbox").gameObject.SetActive(false);
+            punched = false;
+            hitboxCooldown = hitboxCooldownSave;
+        }
+
+		if (Input.GetKeyDown(KeyCode.Mouse0) && attackCooldown <= 0)
+        {
+            Punch();
+        }
+	}
+
+    void Punch()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.Play("skadiAttack");
+
+        //transform.Find("HandsAttackHitbox").gameObject.SetActive(true);
+        attackCooldown = attackCooldownSave;
+        punched = true;
+    }
+}
