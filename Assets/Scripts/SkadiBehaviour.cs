5d2c5677604d575f6100ea1b13fbb07f889cb8eb
SoulPixelIV <william.dj@outlook.de>
2019-01-17 15:18:19 +0100

    Added throwing behaviour to skadis

--- Assets/Scripts/SkadiBehaviour.cs
+++ Assets/Scripts/SkadiBehaviour.cs
@@ -4,37 +4,22 @@ using UnityEngine;
 
 public class SkadiBehaviour : MonoBehaviour {
 
+	public GameObject skadi;
+	public float throwStrength;
     public float attackCooldown;
-    public float hitboxCooldown;
 
     float attackCooldownSave;
-    float hitboxCooldownSave;
     bool punched;
 
-	// Use this for initialization
 	void Start () {
         attackCooldownSave = attackCooldown;
-        hitboxCooldownSave = hitboxCooldown;
     }
-	
-	// Update is called once per frame
+
 	void Update () {
         if (attackCooldown > 0)
         {
             attackCooldown -= 1 * Time.deltaTime;
         }
-
-        if (punched)
-        {
-            hitboxCooldown -= 1 * Time.deltaTime;
-        }
-        if (hitboxCooldown <= 0)
-        {
-            //transform.Find("HandsAttackHitbox").gameObject.SetActive(false);
-            punched = false;
-            hitboxCooldown = hitboxCooldownSave;
-        }
-
 		if (Input.GetKeyDown(KeyCode.Mouse0) && attackCooldown <= 0)
         {
             Punch();
@@ -45,9 +30,10 @@ public class SkadiBehaviour : MonoBehaviour {
     {
         Animator anim = GetComponent<Animator>();
         anim.Play("skadiAttack");
-
-        //transform.Find("HandsAttackHitbox").gameObject.SetActive(true);
         attackCooldown = attackCooldownSave;
         punched = true;
+		//Instantiate Skadi
+		var skadiThrow = Instantiate(skadi, transform.position, Quaternion.identity);
+		skadiThrow.GetComponent<Rigidbody>().AddForce((-transform.right * throwStrength) + new Vector3(0, -10, 0), ForceMode.Impulse);
     }
 }
