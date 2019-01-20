0f331388756b81bc5c306d710677c3a500ed3447
SoulPixelIV <william.priv@outlook.de>
2019-01-20 20:07:35 +0100

    Reworked LiftMovement.cs

--- Assets/Scripts/LiftMovement.cs
+++ Assets/Scripts/LiftMovement.cs
@@ -5,48 +5,22 @@ using UnityEngine;
 public class LiftMovement : MonoBehaviour {
 
     public float travelHeight;
-    public float waitTime;
+    public float travelSpeed;
     public bool active;
 
-    float travelHeightSave;
-    float waitTimeSave;
-    bool waiting;
     int dir;
 
-	void Start () {
-        travelHeightSave = travelHeight;
-        waitTimeSave = waitTime;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    void Start()
+    {
         if (active)
         {
-            travelHeight -= 1 * Time.deltaTime;
+            InvokeRepeating("CallLift", travelHeight, travelHeight);
         }
-        if (travelHeight < 0)
-        {
-            waiting = true;
-        }
-        if (waiting)
-        {
-            waitTime -= 1 * Time.deltaTime;
-        }
-        if (waitTime < 0)
-        {
-            waiting = false;
-            if (dir == 0)
-            {
-                dir = 1;
-            }
-            else
-            {
-                dir = 0;
-            }
-            travelHeight = travelHeightSave;
-            waitTime = waitTimeSave;
-        }
-        if (!waiting && active)
+    }
+
+    void FixedUpdate ()
+    {  
+        if (active)
         {
             if (dir == 0)
             {
@@ -58,4 +32,16 @@ public class LiftMovement : MonoBehaviour {
             }
         }
 	}
+
+    void CallLift()
+    {
+        if (dir == 0)
+        {
+            dir = 1;
+        }
+        else
+        {
+            dir = 0;
+        }
+    }
 }
