c867dd805f4f524a261f32352892830480405ad6
SoulPixelIV <william.priv@outlook.de>
2019-01-28 22:31:25 +0100

    Z-Walkers now aggro when nearby enemies spot you

--- Assets/Scripts/SightChecking.cs
+++ Assets/Scripts/SightChecking.cs
@@ -6,7 +6,6 @@ public class SightChecking : MonoBehaviour {
 
     public bool aggro;
     public float fovAngle = 110;
-	public float nearbyAlertRadius = 0.3f;
 
     SphereCollider col;
     int layerMask;
@@ -27,16 +26,17 @@ public class SightChecking : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
-            Vector3 direction = other.transform.position - transform.position - new Vector3(0, 2, 0);
+            Vector3 direction = other.transform.position - transform.position;
             float angle = Vector3.Angle(direction, transform.forward);
 
             if (angle < fovAngle * 0.5f)
             {
-                Debug.DrawRay(transform.position + new Vector3(0, 2, 0), direction, Color.green);
+                Debug.DrawRay(transform.position + new Vector3(0, 1, 0), direction, Color.green);
                 RaycastHit hit;
 
-                if (Physics.Raycast(transform.position + new Vector3(0, 2, 2), direction.normalized, out hit, col.radius, layerMask))
+                if (Physics.Raycast(transform.position + new Vector3(0, 1, 0), direction.normalized, out hit, col.radius, layerMask))
                 {
+                    Debug.Log(hit.collider);
                     if (hit.collider.gameObject.tag == "Player")
                     {
                         aggro = true;
@@ -45,19 +45,20 @@ public class SightChecking : MonoBehaviour {
             }
             else
             {
-                Debug.DrawRay(transform.position + new Vector3(0, 2, 0), direction, Color.red);
+                Debug.DrawRay(transform.position + new Vector3(0, 1, 0), direction, Color.red);
             }
         }
+
 		if (other.gameObject.tag == "Enemy") 
 		{
-			Vector3 direction = other.transform.position - transform.position - new Vector3(0, 2, 0);
-			float angle = Vector3.Angle(direction, transform.forward);
+			Vector3 directionNew = other.transform.position - transform.position - new Vector3(0, 1, 0);
+			float angleNew = Vector3.Angle(directionNew, transform.forward);
 
-			RaycastHit hit;
+			RaycastHit hit2;
 
-			if (Physics.Raycast(transform.position + new Vector3(0, 2, 2), direction.normalized, out hit, col.radius * nearbyAlertRadius, layerMask))
+			if (Physics.Raycast(transform.position + new Vector3(0, 1, 0), directionNew.normalized, out hit2, col.radius))
 			{
-				if (hit.collider.gameObject.tag == "Enemy" && hit.collider.transform.Find("Sight").GetComponent<SightChecking>().aggro)
+				if (hit2.collider.gameObject.tag == "Enemy" && hit2.collider.transform.Find("Sight").GetComponent<SightChecking>().aggro)
 				{
 					aggro = true;
 				}
