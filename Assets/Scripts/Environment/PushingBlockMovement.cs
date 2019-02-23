a9bd684d1a9bcf7241d687df2c74bf7c24d6d61c
SoulPixelIV <william.priv@outlook.de>
2019-02-23 17:21:29 +0100

    Added moving push blocks

---
+++ Assets/Scripts/Environment/PushingBlockMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingBlockMovement : MonoBehaviour
+{
+
+    public float travelHeight;
+    public float travelSpeed;
+    public bool active;
+
+    int dir;
+    bool activated;
+
+    public void Start()
+    {
+        if (active)
+        {
+            WakeUp();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (active)
+        {
+            if (!activated)
+            {
+                WakeUp();
+            }
+            if (dir == 0)
+            {
+                gameObject.GetComponent<Rigidbody>().MovePosition((transform.position + transform.right * Time.deltaTime * travelSpeed));
+            }
+            else
+            {
+                gameObject.GetComponent<Rigidbody>().MovePosition((transform.position + -transform.right * Time.deltaTime * travelSpeed));
+            }
+        }
+    }
+
+    void CallLift()
+    {
+        if (dir == 0)
+        {
+            dir = 1;
+        }
+        else
+        {
+            dir = 0;
+        }
+    }
+
+    void WakeUp()
+    {
+        InvokeRepeating("CallLift", travelHeight, travelHeight);
+        activated = true;
+    }
+}
+
