c20ed87bdd226d33f62199bf738674a0e9b6b1f4
SoulPixelIV <william.dj@outlook.de>
2019-05-05 18:56:41 +0200

    Continued work on ShoMiRü enemy screen

---
+++ Assets/Scripts/Player/ShoMiRü/ShoMiRüScreen.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoMiRüScreen : MonoBehaviour
+{
+    public float maxRange;
+    public GameObject enemyDot;
+
+    public GameObject player;
+    public GameObject[] enemies;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        InvokeRepeating("ScanForEnemies", 1.0f, 0.1f);
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(transform.rotation.x, player.transform.rotation.y, transform.rotation.z), Time.deltaTime);
+    }
+
+    void ScanForEnemies()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (Vector3.Distance(player.transform.position, enemies[i].transform.position) < maxRange)
+            {
+                if (GameObject.Find("" + enemies[i]) == null)
+                {
+                    GameObject currEnemyDot = Instantiate(enemyDot, transform.position + CalcPos(enemies[i].transform.position), transform.rotation);
+                    currEnemyDot.name = "" + enemies[i];
+                }
+                else
+                {
+                    GameObject targetDot = GameObject.Find("" + enemies[i]);
+                    if (transform.position + (CalcPos(enemies[i].transform.position)) != targetDot.transform.position)
+                    {
+                        targetDot.transform.position = transform.position + (CalcPos(enemies[i].transform.position));
+                    }
+                }
+            }
+            else
+            {
+                if (GameObject.Find("" + enemies[i]) != null)
+                {
+                    GameObject targetDot = GameObject.Find("" + enemies[i]);
+                    Destroy(targetDot);
+                }
+            }
+        }
+    }
+
+    Vector3 CalcPos(Vector3 enemyPos)
+    {
+        Debug.Log("CALC");
+        Vector3 dotPos = new Vector3((player.transform.position.x - enemyPos.x) / 5, (player.transform.position.y - enemyPos.y) / 5, 0);
+        return dotPos;
+    }
+}
